2e1ccdaacdc107dbfd8152c5e98bd3677caf4625
Kapil <kapil@ubuntu>
2014-05-19 01:16:43 +0530

    Script to download files from course webpage

---
+++ abc.py
@@ -0,0 +1,16 @@
+import os
+import re
+import urllib2
+import BeautifulSoup
+
+a=urllib2.urlopen('http://courses.csail.mit.edu/6.852/08/lecture.html').read()
+b=BeautifulSoup.BeautifulSoup(a)
+match = re.compile('\.(ppt|pdf)')
+
+for link in b.findAll('a'):
+  try:
+    href = link['href']
+    if re.search(match, href):
+      os.system('wget http://courses.csail.mit.edu/6.852/08/%s'%href)
+  except KeyError:
+    pass
